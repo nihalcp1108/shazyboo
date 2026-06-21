148ee29fdcb40d5a91ba5d3a3f82fc1b5bffc6a3
WebzioInfo <webzio.info@gmail.com>
2026-06-21 16:09:20 +0530

    sshsshshs

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const API_URL =
   import.meta.env.VITE_API_URL || '/api';
 
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+console.log("API_URL:", API_URL);
+
 export const api = axios.create({
   baseURL: API_URL,
   timeout: 60000,
@@ -11,16 +14,3 @@ export const api = axios.create({
     "Content-Type": "application/json",
   },
 });
-
-// Request interceptor
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-export default api;
