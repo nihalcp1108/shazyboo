8ae7d345ec6048da82204b85659a59b06a1957dc
shabz-seo <shabzcodebrucke@gmail.com>
2026-09-14 16:04:18 +0530

    Implement prerender function for server-side rendering
    Added server-side rendering support with prerender function.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,24 @@ import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </StrictMode>,
-)
+if (typeof document !== 'undefined') {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </StrictMode>,
+  )
+}
+
+export async function prerender(data) {
+  const { renderToString } = await import('react-dom/server')
+  const html = renderToString(
+    <StrictMode>
+      <HelmetProvider>
+        <App url={data.url} />
+      </HelmetProvider>
+    </StrictMode>
+  )
+  return { html }
+}
